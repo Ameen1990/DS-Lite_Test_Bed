d57e5d1e29577522fcaa5f9aaa3650697b1f1273
Ameen Al-Azzawi <45686881+ameen-mcmxc@users.noreply.github.com>
2023-05-09 11:55:47 +0200

    Update b4-tunnel.sh

--- scripts/b4-tunnel.sh
+++ scripts/b4-tunnel.sh
@@ -1,1 +1,18 @@
-#
+#!/bin/bash
+
+# Creating  the main DS-Lite tunnel
+ip link add name ipip6 type ip6tnl local 2001:db8:0:1::2 remote 2001:db8:0:1::1 mode any dev ens39
+
+# Making sure that the interface is up
+ip link set dev ipip6 up
+
+ip route add 198.51.100.0/24 dev ipip6
+
+
+# Enable IP forwaring
+echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 1 > /proc/sys/net/ipv6/conf/all/forwarding
+
+# Flushing iptables rules
+iptables -F
+iptables -X
